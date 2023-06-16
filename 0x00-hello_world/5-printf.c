76f591634dad048dd487a9ef2f4c4018b68536d3
YonelaLisah <mjanyelwayonela@gmail.com>
2023-06-16 13:24:22 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  * main - An entry for a program that prints a line with the function printf
  * Return: 0
  */
-
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n")
+printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
